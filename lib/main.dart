3ee9314ed012419af87748901076730306b9ecda
Josep <josepcapotorres@gmail.com>
2026-08-13 10:23:46 +0200

    feat(firebase): Integrate Firebase Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,14 +10,27 @@ import 'package:path_provider/path_provider.dart';
 
 import 'core/routes/app_routes.dart';
 import 'core/themes/app_themes.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await SystemChrome.setPreferredOrientations([.portraitUp, .portraitDown]);
 
   final dir = await getApplicationDocumentsDirectory();
   Hive.init(dir.path);
+
+  FlutterError.onError = (errorDetails) {
+    // Send errors run in sync functions / methods
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    // Send errors run in async functions / methods
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 
   runApp(const MyApp());
 }
