c8ef529d95a23d3ec680674bed948ecad113eabc
Josep <josepcapotorres@gmail.com>
2026-08-20 10:05:40 +0200

    chore(app): Change the package names on Android and iOS to proper ones

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.focus_box"
+    namespace = "es.josepcapo.focusbox"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.focus_box"
+        applicationId = "es.josepcapo.focusbox"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
