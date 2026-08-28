061f22218b7db49b7d389393bd707c04980c2b84
Josep <josepcapotorres@gmail.com>
2026-08-28 15:55:53 +0200

    feat(focus_mode): Add inheritance to FocusSessionEntity with Equatable

--- lib/features/focus_mode/domain/entities/focus_session_entity.dart
+++ lib/features/focus_mode/domain/entities/focus_session_entity.dart
@@ -1,12 +1,17 @@
+import 'package:equatable/equatable.dart';
+
 import '../../../../core/domain/enums/task_status.dart';
 
-class FocusSessionEntity {
+class FocusSessionEntity extends Equatable {
   final String taskId;
   final TaskStatus status;
 
-  FocusSessionEntity({required this.taskId, required this.status});
+  const FocusSessionEntity({required this.taskId, required this.status});
 
   FocusSessionEntity copyWith({TaskStatus? status}) {
     return FocusSessionEntity(taskId: taskId, status: status ?? this.status);
   }
+
+  @override
+  List<Object?> get props => [taskId, status];
 }
